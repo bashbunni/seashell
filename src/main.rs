4566b785accf7ed6fc1d5e4af3c9e42d932df1b9
bashbunni <bunni@bashbunni.dev>
2026-05-13 13:35:51 -0700

    chore: remove comment

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@
 use std::io::{self, Write};
 
 fn main() {
-    // TODO: Uncomment the code below to pass the first stage
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
@@ -17,8 +16,4 @@ fn main() {
             _ => println!("{}: command not found", command.trim()),
         }
     }
-    // 1. Read: Display a prompt and wait for user input
-    // 2. Eval: Parse and execute the command
-    // 3. Print: Display the output or error message
-    // 4. Loop: Return to step 1 and wait for the next command
 }
