4e3cc46397c829c0ee4557d34541c7e847100cb1
bashbunni <bunni@bashbunni.dev>
2026-05-13 12:51:42 -0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,9 @@ fn main() {
     // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    let mut command = String::new();
+    match io::stdin().read_line(&mut command) {
+        _ => print!("{}: command not found", command.trim()),
+    };
 }
