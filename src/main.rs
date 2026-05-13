eac7a897b1e16b96963251b5e2e6e4d288d968f7
bashbunni <bunni@bashbunni.dev>
2026-05-13 15:44:56 -0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::str::FromStr;
+use strum_macros::EnumString;
 
 fn main() {
     loop {
@@ -11,12 +13,33 @@ fn main() {
             .read_line(&mut input)
             .expect("unable to read user input");
         // we are okay to panic on this failure right now.
-        let (command, remainder) = input.split_once(" ").unwrap_or((&input, "")); // TODO if ok, then use it...
 
-        match command.trim() {
-            "exit" => return,
-            "echo" => println!("{}", remainder.trim()),
+        let (command, remainder) = input.split_once(" ").unwrap_or((&input, ""));
+
+        match Command::from_str(command.trim()) {
+            Ok(Command::Exit) => return,
+            Ok(Command::Echo) => println!("{}", remainder.trim()),
+            Ok(Command::Type) => match Command::from_str(remainder.trim()) {
+                Ok(_) => println!("{} is a shell builtin.", remainder.trim()),
+                Err(_) => println!("{}: command not found", remainder.trim()),
+            },
             _ => println!("{}: command not found", input.trim()),
         }
     }
 }
+
+#[derive(EnumString)]
+enum Command {
+    #[strum(serialize = "exit")]
+    Exit,
+    #[strum(serialize = "echo")]
+    Echo,
+    #[strum(serialize = "type")]
+    Type,
+}
+
+// I need some way to check if the command exists. I want to use pattern matching.
+//
+// map{ command: function } -> all of the available commands
+// when I get type, I see if the remainder exists
+// current match restrictions: it's only on command.trim() that we're matching, not callable.
