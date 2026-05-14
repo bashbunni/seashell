862f94279580953f8837fa1b88b0d467b19fce39
bashbunni <bunni@bashbunni.dev>
2026-05-14 11:53:09 -0700

    fix: exit uses process::exit(0)

--- src/main.rs
+++ src/main.rs
@@ -44,7 +44,7 @@ fn eval(input: &str) {
     let (command, remainder) = input.split_once(" ").unwrap_or((&input, ""));
 
     match Command::from_str(command.trim()) {
-        Ok(Command::Exit) => return,
+        Ok(Command::Exit) => std::process::exit(0),
         Ok(Command::Echo) => Command::handle_echo(remainder.trim()),
         Ok(Command::Type) => Command::handle_type(remainder.trim()),
         _ => println!("{}: command not found", input.trim()),
