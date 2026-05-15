d459801961fd7afc0eabddf8d8f50a3abd6c6ba6
bashbunni <bunni@bashbunni.dev>
2026-05-14 21:46:04 -0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -19,15 +19,19 @@ use strum_macros::EnumString;
 fn main() {
     // eval loop
     loop {
-        print!("$ ");
-        io::stdout().flush().unwrap();
-
-        let mut input = String::new();
-        io::stdin()
-            .read_line(&mut input)
-            .expect("unable to read user input");
-        eval(&input);
+        run()
     }
+}
+
+fn run() {
+    print!("$ ");
+    io::stdout().flush().unwrap();
+
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("unable to read user input");
+    eval(&input);
 }
 
 #[derive(EnumString)]
@@ -38,6 +42,8 @@ enum Command {
     Echo,
     #[strum(serialize = "type")]
     Type,
+    #[strum(serialize = "\n")]
+    Enter,
 }
 
 impl Command {
@@ -57,6 +63,7 @@ fn find_executable(input: &str) -> Option<PathBuf> {
     env::var_os("PATH").and_then(|path| {
         env::split_paths(&path).find_map(|dir| {
             let exec_path = dir.join(input);
+            // TODO I think is_executable fails if the args are invalid
             exec_path.is_executable().then_some(exec_path)
         })
     })
@@ -64,17 +71,28 @@ fn find_executable(input: &str) -> Option<PathBuf> {
 
 // evaluate commands
 fn eval(input: &str) {
-    let (command, remainder) = input.split_once(" ").unwrap_or((input, ""));
+    let (mut command, mut remainder) = input.split_once(" ").unwrap_or((input, ""));
 
-    match Command::from_str(command.trim()) {
+    // do nothing if they hit enter.
+    if let Ok(Command::Enter) = Command::from_str(command) {
+        return;
+    }
+
+    command = command.trim();
+    remainder = remainder.trim();
+
+    match Command::from_str(command) {
         Ok(Command::Exit) => std::process::exit(0),
-        Ok(Command::Echo) => println!("{}", remainder.trim()),
-        Ok(Command::Type) => Command::handle_type(remainder.trim()),
-        _ => match find_executable(command) {
+        Ok(Command::Echo) => println!("{}", remainder),
+        Ok(Command::Type) => Command::handle_type(remainder),
+        _ => match find_executable(command.trim()) {
             Some(exec_path) => {
-                let args: Vec<&str> = remainder.split(" ").collect();
-                let err = std::process::Command::new(exec_path).args(args).exec(); // apparently if this returns, then there was a problem.
-                println!("unable to execute command: {err}");
+                println!("{}", exec_path.display());
+                let args: Vec<&str> = remainder.split(" ").map(|x| x.trim()).collect();
+                match std::process::Command::new(exec_path).args(args).spawn() {
+                    Ok(_) => println!("success!"),
+                    Err(err) => println!("unable to execute command: {err}"),
+                }
             }
             None => println!("{}: command not found", input.trim()),
         },
